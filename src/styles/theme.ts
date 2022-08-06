7141d67303e1cab8e2a2d8d6940e1e277cd88342
joaobr200 <joaoarantesjob@gmail.com>
2022-08-06 01:28:53 -0300

    add: default theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { blueA, grayA, grassA, redA, orangeA } from "@radix-ui/colors";
+
+export const theme = {
+  white: "#F9F9F9",
+  black: "#090909",
+  blue: {
+    9: blueA.blueA9,
+    10: blueA.blueA10,
+    11: blueA.blueA11,
+    12: blueA.blueA12,
+  },
+  gray: {
+    5: grayA.grayA5,
+    6: grayA.grayA6,
+    7: grayA.grayA7,
+    8: grayA.grayA8,
+    9: grayA.grayA9,
+    10: grayA.grayA10,
+  },
+  success: grassA.grassA9,
+  error: redA.redA9,
+  warning: orangeA.orangeA9,
+};
